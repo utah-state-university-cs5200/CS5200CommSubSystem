fb3ed2eccfe2e5d5fbd3c7439ce799041adcba9e
aayush goyal <aayushgoyal9507@gmail.com>
2019-03-13 16:20:10 -0600

    LoginUser added
    LoginUser message class

--- src/com/hw2/LoginUserMessage.java
+++ src/com/hw2/LoginUserMessage.java
@@ -1,7 +1,36 @@
 package com.hw2;
 
-public class LoginUserMessage {
+import java.io.IOException;
+
+public class LoginUserMessage extends message{
+
+    private String email;
+    private String password;
+
+
+    public LoginUserMessage(String email, String password){
+        this.email=email;
+        this.password=password;
+    }
+
     public static message decode(byte[] messageBytes) {
-        return null;
+        message.Decoder decoder = new message.Decoder(messageBytes);
+
+        if (decoder.decodeMessageType() != MessageType.LoginUser) {
+            throw new IllegalArgumentException();
+        }
+
+        String email = decoder.decodeString();
+        String password = decoder.decodeString();
+
+        return new LoginUserMessage(email, password);
+    }
+
+    @Override
+    public byte[] encode() throws IOException {
+        return new Encoder()
+                .encodeString(email)
+                .encodeString(password)
+                .toByteArray();
     }
 }
