b89229396c96c1d798d38843d3bccd4b8c72922b
aayush goyal <aayushgoyal9507@gmail.com>
2019-03-13 03:20:45 -0600

    Create product added

--- src/com/hw2/CreateProdMessage.java
+++ src/com/hw2/CreateProdMessage.java
@@ -1,11 +1,52 @@
 package com.hw2;
 
-public class CreateProdMessage {
-    private short msgType;
-    RegisterUserMessage user;
-    ProductMessage prod;
+import java.io.IOException;
+
+public class CreateProdMessage extends message{
+
+    private short userId;
+    private String name;
+    private String prodId;
+    private short price;
+
+    public CreateProdMessage(short userid, String name, String prodId, short price){
+        this.userId=userid;
+        this.name=name;
+        this.prodId=prodId;
+        this.price=price;
+
+    }
+
+
+
+
 
     public static message decode(byte[] messageBytes) {
-        return null;
+        Decoder decoder = new Decoder(messageBytes);
+
+        if (decoder.decodeMessageType() != MessageType.RegisterUser) {
+            throw new IllegalArgumentException();
+        }
+
+        short userId = decoder.decodeShort();
+        String name = decoder.decodeString();
+        String prodId = decoder.decodeString();
+        short price = decoder.decodeShort();
+
+        return new CreateProdMessage(userId, name, prodId, price);
     }
+
+    @Override
+    public byte[] encode() throws IOException {
+        return new Encoder()
+                .encodeMessageType(messageType)
+                .encodeShort(userId)
+                .encodeString(name)
+                .encodeString(prodId)
+                .encodeShort(price)
+                .toByteArray();
+
+    }
+
+
 }
